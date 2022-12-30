a60b56fd22f6969fc759b6045e15a6a8cd76b8ca
benjithorpe <benthorpe.dev@gmail.com>
2022-12-30 06:52:50 +0000

    refactor menu items

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,7 +1,65 @@
 <script>
-	import Menu from '$lib/components/home/OurMenu.svelte';
+	import BreakfastCard from '../../lib/components/BreakfastCard.svelte';
+
+	const breakfasts = [
+		{
+			name: 'Egg Sandwich',
+			contents: 'Meat Ball, Mie',
+			price: 30
+		},
+		{
+			name: 'Egg Sandwich',
+			contents: 'Meat Ball, Mie',
+			price: 30
+		},
+		{
+			name: 'Egg Sandwich',
+			contents: 'Meat Ball, Mie',
+			price: 30
+		},
+		{
+			name: 'Egg Sandwich',
+			contents: 'Meat Ball, Mie',
+			price: 30
+		},
+		{
+			name: 'Egg Sandwich',
+			contents: 'Meat Ball, Mie',
+			price: 30
+		},
+		{
+			name: 'Egg Sandwich',
+			contents: 'Meat Ball, Mie',
+			price: 30
+		},
+		{
+			name: 'Egg Sandwich',
+			contents: 'Meat Ball, Mie',
+			price: 30
+		},
+		{
+			name: 'Egg Sandwich',
+			contents: 'Meat Ball, Mie',
+			price: 30
+		},
+		{
+			name: 'Egg Sandwich',
+			contents: 'Meat Ball, Mie',
+			price: 30
+		}
+	];
 </script>
 
-<Menu />
+<article class="container mx-auto min-h-screen py-16 px-2 lg:max-w-6xl">
+	<!-- Banner -->
+	<div class="mb-12 text-center">
+		<p class="text-base text-red-400">Specialities</p>
+		<h2 class="text-2xl font-bold">Our Menu</h2>
+	</div>
 
-<style></style>
+	<section class="menu grid lg:grid-cols-3 lg:gap-10">
+		{#each breakfasts as breakfast}
+			<BreakfastCard {breakfast} />
+		{/each}
+	</section>
+</article>
