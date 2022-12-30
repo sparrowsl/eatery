2d606904e210c48afb3ad7f61877d659eeebb5a0
benjithorpe <benthorpe.dev@gmail.com>
2022-12-30 06:07:59 +0000

    use the 'each' syntax to display cards

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,6 +1,44 @@
-<script></script>
+<script>
+	import 'iconify-icon';
 
-<section class="container mx-auto min-h-[90vh] py-16 lg:max-w-6xl">
+	const staff = [
+		{
+			name: 'Aaron Patel',
+			position: 'CEO',
+			image: 'chef-1.jpg',
+			socials: [
+				{ icon: 'mdi:twitter', url: '' },
+				{ icon: 'mdi:facebook', url: '' },
+				{ icon: 'material-symbols:mail', url: '' },
+				{ icon: 'mdi:instagram', url: '' }
+			]
+		},
+		{
+			name: 'Daniel Tebas',
+			position: 'Chef',
+			image: 'chef-2.jpg',
+			socials: [
+				{ icon: 'mdi:twitter', url: '' },
+				{ icon: 'mdi:facebook', url: '' },
+				{ icon: 'material-symbols:mail', url: '' },
+				{ icon: 'mdi:instagram', url: '' }
+			]
+		},
+		{
+			name: 'John Snow',
+			position: 'Chef',
+			image: 'chef-3.jpg',
+			socials: [
+				{ icon: 'mdi:twitter', url: '' },
+				{ icon: 'mdi:facebook', url: '' },
+				{ icon: 'material-symbols:mail', url: '' },
+				{ icon: 'mdi:instagram', url: '' }
+			]
+		}
+	];
+</script>
+
+<article class="container mx-auto min-h-[90vh] py-16 lg:max-w-6xl">
 	<div class="mb-12 text-center">
 		<p class="text-base text-red-400">Specialities</p>
 		<h1 class="text-2xl font-bold">Our Team</h1>
@@ -8,51 +46,20 @@
 
 	<!-- Team members -->
 	<section class="grid gap-10 md:grid-cols-3 md:gap-5 lg:px-10">
-		<figure class="px-4">
-			<img src="/img/chef-1.jpg" alt="" class="" />
-			<figcaption class="text-center">
-				<h3 class="font-josefin mt-1 text-2xl font-normal">Aaron Patel</h3>
-				<p class="font-light">CEO</p>
-				<!-- Social contacts -->
-				<div class="mt-2 flex justify-center gap-5 text-xl text-red-500">
-					<a href=""><i class="ri-twitter-fill" /></a>
-					<a href=""><i class="ri-facebook-fill" /></a>
-					<a href=""><i class="ri-mail-fill" /></a>
-					<a href=""><i class="ri-instagram-line" /></a>
-				</div>
-			</figcaption>
-		</figure>
-
-		<figure class="px-4">
-			<img src="/img/chef-2.jpg" alt="" />
-			<figcaption class="text-center">
-				<h3 class="font-josefin mt-1 text-2xl font-normal">Daniel Tebas</h3>
-				<p class="font-light">Chef</p>
-				<!-- Social contacts -->
-				<div class="mt-2 flex justify-center gap-5 text-xl text-red-500">
-					<a href=""><i class="ri-twitter-fill" /></a>
-					<a href=""><i class="ri-facebook-fill" /></a>
-					<a href=""><i class="ri-mail-fill" /></a>
-					<a href=""><i class="ri-instagram-line" /></a>
-				</div>
-			</figcaption>
-		</figure>
-
-		<figure class="px-4">
-			<img src="/img/chef-3.jpg" alt="" />
-			<figcaption class="text-center">
-				<h3 class="font-josefin mt-1 text-2xl font-normal">Jon Snow</h3>
-				<p class="font-light">Chef</p>
-				<!-- Social contacts -->
-				<div class="mt-2 flex justify-center gap-5 text-xl text-red-500">
-					<a href=""><i class="ri-twitter-fill" /></a>
-					<a href=""><i class="ri-facebook-fill" /></a>
-					<a href=""><i class="ri-mail-fill" /></a>
-					<a href=""><i class="ri-instagram-line" /></a>
-				</div>
-			</figcaption>
-		</figure>
+		{#each staff as user (user)}
+			<figure class="px-4">
+				<img src={`/img/${user.image}`} alt="" class="" />
+				<figcaption class="text-center">
+					<h3 class="font-josefin mt-1 text-2xl font-normal">{user.name}</h3>
+					<p class="font-light">{user.position}</p>
+					<!-- Social contacts -->
+					<div class="mt-2 flex justify-center gap-5 text-xl text-red-500">
+						{#each user.socials as social}
+							<a href={social.url}><iconify-icon icon={social.icon} /></a>
+						{/each}
+					</div>
+				</figcaption>
+			</figure>
+		{/each}
 	</section>
-</section>
-
-<style></style>
+</article>
